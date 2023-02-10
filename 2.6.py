d08a8bb869c6049d0480a1e23389e25f75e9d34d
Maryna Lukash <marishkapostolnaya@gmail.com>
2023-02-10 08:35:13 +0100

    Create project

---
+++ 2.6.py
@@ -0,0 +1,4 @@
+import math
+
+r = int(input('Введіть радіус кола: '))
+print('Площа кола дорівнює:', math.pi * (r ** 2))
---
+++ 2.7.py
@@ -0,0 +1,6 @@
+import math
+
+lenght = 700
+velocity = 90
+driving_time = math.ceil(lenght / velocity)
+print(driving_time)
